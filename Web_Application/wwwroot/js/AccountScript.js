71dd18557393595ae7a91d3b394aa38bd810d454
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-06-21 17:37:17 +0300

    Modifie style and script

--- Web_Application/wwwroot/js/AccountScript.js
+++ Web_Application/wwwroot/js/AccountScript.js
@@ -7,6 +7,8 @@
          password.style.display = "none";
          tfa.style.display = "none";
          verification.style.display = "none";
+         disable_tfa.style.display = "none";
+         enable_tfa.style.display = "none";
          profile.style.display = "block";
 
             } else {
@@ -24,6 +26,8 @@ function showemail() {
         password.style.display = "none";
         tfa.style.display = "none";
         verification.style.display = "none";
+        disable_tfa.style.display = "none";
+        enable_tfa.style.display = "none";
         email.style.display = "block";
     
     } else {
@@ -39,27 +43,54 @@ function showpassword() {
         tfa.style.display = "none";
         email.style.display = "none";
         verification.style.display = "none";
+        disable_tfa.style.display = "none";
+        enable_tfa.style.display = "none";
         password.style.display = "block";
     } else {
         password.style.display = "none";
     }
 }
 
-function show2fa() {
-   
-
-   
+function show2fa() { 
     if (tfa.style.display === "none") {
         profile.style.display = "none";       
         email.style.display = "none";
         password.style.display = "none";
         verification.style.display = "none";
+        disable_tfa.style.display = "none";
+        enable_tfa.style.display = "none";
         tfa.style.display = "block";
     } else {
         tfa.style.display = "none";
     }
 }
 
+function enabletfa() {
+    if (enable_tfa.style.display === "none") {
+        profile.style.display = "none";
+        email.style.display = "none";
+        password.style.display = "none";
+        verification.style.display = "none";
+        tfa.style.display = "none";
+        disable_tfa.style.display = "none";
+        enable_tfa.style.display = "block";
+    } else {
+        enable_tfa.style.display = "none";
+    }
+}
 
+function disabletfa() {
+    if (disable_tfa.style.display === "none") {
+        profile.style.display = "none";
+        email.style.display = "none";
+        password.style.display = "none";
+        verification.style.display = "none";
+        disable_tfa.style.display = "block";
+        enable_tfa.style.display = "none";
+        tfa.style.display = "none";
+    } else {
+        disable_tfa.style.display = "none";
+    }
+}
 
 
