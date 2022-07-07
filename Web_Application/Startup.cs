788925e12fc4955e08ff49a6af2ad7fd2233ac9c
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-07-07 16:17:37 +0300

    connected service multi language

--- Web_Application/Startup.cs
+++ Web_Application/Startup.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Web_Application.Models;
@@ -28,6 +32,26 @@ namespace Web_Application
             services.AddHttpContextAccessor();
             //implement DB
             services.AddDbContext<ConString>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //Implement resource files for Multilanguage
+            services.AddLocalization(opt=> { opt.ResourcesPath = "Resources"; });
+            services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+
+            services.Configure<RequestLocalizationOptions>(
+                opt =>
+                {
+                    var supportedCultures = new List<CultureInfo>
+                    {
+                       new CultureInfo("en"),
+                    new CultureInfo("ru")
+                };
+                    opt.DefaultRequestCulture = new RequestCulture("en");
+                    opt.SupportedCultures = supportedCultures;
+                    opt.SupportedUICultures = supportedCultures;
+                }
+                );
+
+            //By default services
             services.AddControllersWithViews();
         }
 
@@ -48,6 +72,9 @@ namespace Web_Application
 
             app.UseAuthorization();
 
+            app.UseRequestLocalization(app.ApplicationServices.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+            
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
